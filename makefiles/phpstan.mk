d0e06e15e7e3dc93e0383e10eb019fc00b3b4bdd
Florian Abélard <f.abelard.dev@gmail.com>
2022-11-20 23:23:52 +0100

    chore: configure phpstan

--- makefiles/phpstan.mk
+++ makefiles/phpstan.mk
@@ -2,7 +2,7 @@
 # PHPStan Makefile
 #------------------------------------------------------------------------------
 
-PHPSTAN_DOCKER_CMD = docker-compose -f ${DOCKER_COMPOSE_ANALYSER_FILE} run -T phpstan ${1}
+PHPSTAN_DOCKER_CMD = docker-compose -f ${DOCKER_COMPOSE_ANALYSER_FILE} run --rm -T phpstan ${1}
 
 # Cli arguments
 ifneq (,$(filter phpstan-analyse%, $(firstword $(MAKECMDGOALS))))
@@ -13,7 +13,10 @@ endif
 #------------------------------------------------------------------------------
 
 phpstan-analyse: ##@phpstan run the analysis on the specified folder
-	$(call PHPSTAN_DOCKER_CMD, analyse -c /app/docker/images/phpstan/phpstan.neon --level=6 $(COMPOSER_CLI_ARGS))
+	$(call PHPSTAN_DOCKER_CMD, analyse -c /app/docker/images/phpstan/phpstan.neon $(COMPOSER_CLI_ARGS))
+
+phpstan-rebuild: ##@phpstan rebuild the phpstan image
+	docker-compose -f ${DOCKER_COMPOSE_ANALYSER_FILE} build phpstan
 
 #------------------------------------------------------------------------------
 
