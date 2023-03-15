dde656b738fa1d6afcebc56e2333aad7d5c21797
Florian Abélard <f.abelard.dev@gmail.com>
2023-03-15 22:50:24 +0100

    feat: install maker bundle

--- application/backend/config/bundles.php
+++ application/backend/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
