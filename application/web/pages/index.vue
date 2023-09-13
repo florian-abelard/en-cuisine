7c159156ed1ccf99ae594bac5479be8190f61ab7
Florian Abélard <f.abelard.dev@gmail.com>
2023-09-13 23:13:56 +0200

    feat: premiere page nuxt

---
+++ application/web/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Index page</h1>
+
+  <div>{{ count }}</div>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
+
+<script setup lang="ts">
+  const count = ref(0);
+  const increment = () => count.value++;
+  const decrement = () => count.value--;
+</script>
