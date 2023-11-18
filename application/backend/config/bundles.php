49a0cf2841ed563b0d139127dc3d246fb7ae3d18
Florian Abélard <f.abelard.dev@gmail.com>
2023-11-18 19:18:47 +0100

    chore: install foundry

--- application/backend/config/bundles.php
+++ application/backend/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -2,19 +2,20 @@
 # Docker Makefile
 #------------------------------------------------------------------------------
 
-DOCKER_PHP_CMD = docker compose exec -T --user ${USER_ID}:${GROUP_ID} php ${1}
+DOCKER_CMD = docker compose exec -T --user ${USER_ID}:${GROUP_ID} ${1}
 
 # Cli arguments
-ifneq (,$(filter cmd-%, $(firstword $(MAKECMDGOALS))))
+ifneq (,$(filter cmd-php, $(firstword $(MAKECMDGOALS))))
     CLI_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
-    $(eval $(CLI_ARGS):;@:)
+	CLI_ARGS := $(subst :,\:,$(CLI_ARGS))
+    $(eval $(CLI_ARGS):;@true)
 endif
 
 #------------------------------------------------------------------------------
 
 cmd-php: ##@docker run the specified command in the php container | usage: make cmd-php -- COMMAND
 	@echo ${CLI_ARGS}
-	@$(call DOCKER_PHP_CMD, $(CLI_ARGS))
+	@$(call DOCKER_CMD, php $(CLI_ARGS))
 
 #------------------------------------------------------------------------------
 
