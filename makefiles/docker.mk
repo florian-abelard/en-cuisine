3124682fdb7cca21ce399b6c1a21a20acbc21e39
Florian Abélard <f.abelard.dev@gmail.com>
2024-01-03 23:33:09 +0100

    chore: fix docker cmd

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -2,13 +2,13 @@
 # Docker Makefile
 #------------------------------------------------------------------------------
 
-DOCKER_CMD = docker compose exec -T --user ${USER_ID}:${GROUP_ID} -- ${1}
+DOCKER_CMD = docker compose exec -T --user ${USER_ID}:${GROUP_ID} ${1}
 
 # Cli arguments
 ifneq (,$(filter cmd-php, $(firstword $(MAKECMDGOALS))))
     CLI_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
     CLI_ARGS := $(subst :,\:,$(CLI_ARGS))
-    $(eval $(CLI_ARGS):;@true)
+    $(foreach arg,$(CLI_ARGS),$(eval $(arg):;@true))
 endif
 
 #------------------------------------------------------------------------------
@@ -16,6 +16,10 @@ endif
 cmd-php: ##@docker run the specified command in the php container | usage: make cmd-php -- COMMAND
 	@echo ${CLI_ARGS}
 	@$(call DOCKER_CMD, php $(CLI_ARGS))
+
+cmd-node: ##@docker run the specified command in the php container | usage: make cmd-node -- COMMAND
+	@echo ${CLI_ARGS}
+	@$(call DOCKER_CMD, node $(CLI_ARGS))
 
 #------------------------------------------------------------------------------
 
@@ -26,7 +30,6 @@ rebuild: build up ##@docker rebuild and start containers
 
 clean: down ##@docker clean docker containers
 	docker container ls -a | grep "${APP_NAME}" | awk '{print $1}' | xargs --no-run-if-empty docker container rm
-#   docker image rm $(docker images -a -q)
 
 #------------------------------------------------------------------------------
 
