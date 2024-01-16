246a9eaf6a4dae9e32bd11032c661a8348c44200
Florian Abélard <f.abelard.dev@gmail.com>
2024-01-17 00:07:03 +0100

    feat: ajout pagination WIP

--- application/web/pages/recette/list.vue
+++ application/web/pages/recette/list.vue
@@ -7,6 +7,22 @@
         {{ index }} - {{ recette.libelle }}
       </li>
     </ul>
+
+    <button
+      @click="previous"
+      class="btn mx-2"
+    >
+      Précendente
+    </button>
+
+    Page : {{ page }}
+
+    <button
+      @click="next"
+      class="btn mx-2"
+    >
+      Suivante
+    </button>
   </div>
 </template>
 
@@ -15,18 +31,31 @@
   import { useFetch } from '#imports';
   import { useRuntimeConfig } from '#imports';
   import { useQuery } from '#imports';
+  import { ref } from '#imports';
 
   const config = useRuntimeConfig();
+  const page = ref(1);
+
+
+  const next = () => {
+    page.value++;
+  }
+
+  const previous = () => {
+    page.value--;
+  }
 
   const { data: recettes }= useQuery({
-    queryKey: ['recettes'],
+    queryKey: ['recettes', page],
     queryFn: fetchRecettes,
   })
 
-  async function fetchRecettes () {
+  async function fetchRecettes() {
+    console.log(page.value);
     return await useFetch('/recettes', {
       method: 'get',
       baseURL: config.public.apiBaseUrl,
+      query: { page }
     })
     .then((response) => response.data.value['hydra:member'])
     .catch((error) => console.log(error));
