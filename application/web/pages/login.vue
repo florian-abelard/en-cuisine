c0b9f56c08054e1b5320f6607cd121a683815181
Florian Abélard <f.abelard.dev@gmail.com>
2024-02-04 23:16:12 +0100

    feat: login component WIP

---
+++ application/web/pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-3xl font-bold">
+      Bienvenue sur l'application En Cuisine !
+    </h1>
+
+    <button
+      type="submit"
+      class="btn btn-primary mx-2"
+    >
+      Increment
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
